b598ba96e0e11bc76d4995ede69d1164e209ff12
new-sankaku <sakurasakusankaku@gmail.com>
2024-04-28 19:57:50 +0900

    Init

---
+++ js/Settings.js
@@ -0,0 +1,6 @@
+var canvasFullWidth = 750;
+var canvasFullHeight = 900;
+var maxAreaWidth = canvasFullWidth - 100;
+var maxAreaHeight = canvasFullHeight - 100;
+var clipAreaCoords = { left: 50, top: 50, width: maxAreaWidth, height: maxAreaHeight };
+var svgPagging = 50;

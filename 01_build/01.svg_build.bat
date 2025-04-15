34d17d891be83d1c97c4bc6ba3872081d387d7f0
new-sankaku <sakurasakusankaku@gmail.com>
2025-04-15 20:47:58 +0900

    移転、Repository Move

--- 01_build/01.svg_build.bat
+++
@@ -1,4 +0,0 @@
-python SvgBuilder.py ../02_images_svg/MangaPanelsImage/Landscape ../js/svg/manga-panels-image-landscape.js MangaPanelsImage_Landscape
-python SvgBuilder.py ../02_images_svg/MangaPanelsImage/Vertical ../js/svg/manga-panels-image-vertical.js MangaPanelsImage_Vertical
-python SvgBuilder.py ../02_images_svg/SpeechBubble ../js/svg/speechbubble.js SpeechBubble
-PAUSE echo end.
--- json_js/00_prompt_sdxl_base.js
+++
@@ -1,6 +0,0 @@
-var prompt_sdxl_base = {
-	// "horizontalLine@2":{},
-	// "sdxl": {
-	// 	"sdxl111": 		{"url": "03_images/imgPromptHelper/sdxl111.webp" }
-	// }
-}
